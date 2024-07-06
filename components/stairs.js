d62e179d026682eadcb0e49f02398623b90c0975
harshil5506 <harshil.hitendra.may2006@gmail.com>
2024-07-07 00:55:38 +0530

    stairs 3

---
+++ components/stairs.js
@@ -0,0 +1,64 @@
+// components/SvgComponent.js
+
+import styles from './globals.css';
+
+const stairs = () => {
+  return (
+    <div className={styles.svgContainer}>
+      <svg width="1277" height="1743" viewBox="0 0 1277 1743" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M379.453 0.371094L20.1876 128.018L360.241 266.911L717.584 135.89L379.453 0.371094Z" fill="#0C8ABD"/>
+        <path d="M360.241 312.459V266.911L717.585 135.89V181.438L360.241 312.459Z" fill="#BEE8F9"/>
+        <path d="M423 384.435V338.887L780.344 207.867V253.415L423 384.435Z" fill="#BEE8F9"/>
+        <path d="M485.759 455.288V409.74L843.103 278.719V324.267L485.759 455.288Z" fill="#BEE8F9"/>
+        <path d="M548.519 526.14V480.592L905.862 349.572V395.119L548.519 526.14Z" fill="#BEE8F9"/>
+        <path d="M423 338.888L360.241 312.458L717.585 181.438L780.344 207.867L423 338.888Z" fill="#0C8ABD"/>
+        <path d="M485.759 410.302L423 383.873L780.344 252.853L843.103 279.282L485.759 410.302Z" fill="#0C8ABD"/>
+        <path d="M548.519 481.154L485.759 454.725L843.103 323.705L905.862 350.134L548.519 481.154Z" fill="#0C8ABD"/>
+        <path d="M357.039 787.619L0.335205 905.706L369.847 1070.47L727.19 939.445L357.039 787.619Z" fill="#0C8ABD"/>
+        <path d="M369.847 1116.01V1070.47L727.191 939.445V984.993L369.847 1116.01Z" fill="#BEE8F9"/>
+        <path d="M432.606 1187.99V1142.44L789.95 1011.42V1056.97L432.606 1187.99Z" fill="#BEE8F9"/>
+        <path d="M495.365 1258.84V1213.29L852.709 1082.27V1127.82L495.365 1258.84Z" fill="#BEE8F9"/>
+        <path d="M558.125 1329.69V1284.15L915.469 1153.13V1198.67L558.125 1329.69Z" fill="#BEE8F9"/>
+        <path d="M432.606 1142.44L369.847 1116.01L727.191 984.993L789.95 1011.42L432.606 1142.44Z" fill="#0C8ABD"/>
+        <path d="M495.365 1213.86L432.606 1187.43L789.95 1056.41L852.709 1082.84L495.365 1213.86Z" fill="#0C8ABD"/>
+        <path d="M558.125 1284.71L495.365 1258.28L852.709 1127.26L915.468 1153.69L558.125 1284.71Z" fill="#0C8ABD"/>
+        <path d="M905.862 395.12L548.519 526.14L919.311 677.966L1271.53 545.259L905.862 395.12Z" fill="#DD6702"/>
+        <path d="M548.519 573.937V526.14L919.311 677.966V726.326L548.519 573.937Z" fill="#FFDE88"/>
+        <path d="M484.479 645.914V598.117L855.271 749.943V798.303L484.479 645.914Z" fill="#FFDE88"/>
+        <path d="M420.438 716.766V668.969L791.231 820.796V869.155L420.438 716.766Z" fill="#FFDE88"/>
+        <path d="M356.398 787.619V739.822L727.19 891.648V940.008L356.398 787.619Z" fill="#FFDE88"/>
+        <path d="M484.479 598.117L548.519 573.937L919.311 726.326L855.271 749.943L484.479 598.117Z" fill="#DD6702"/>
+        <path d="M420.438 669.532L484.479 645.352L855.271 797.74L791.231 821.358L420.438 669.532Z" fill="#DD6702"/>
+        <path d="M356.398 740.384L420.438 716.204L791.231 868.593L727.19 892.21L356.398 740.384Z" fill="#DD6702"/>
+        <path d="M915.469 1198.67L558.125 1329.13L928.277 1480.96L1276.65 1337L915.469 1198.67Z" fill="#DD6702"/>
+        <path d="M557.484 1376.93L558.125 1329.13L928.276 1480.96V1529.32L557.484 1376.93Z" fill="#FFDE88"/>
+        <path d="M493.444 1448.91V1401.11L864.236 1552.94V1601.3L493.444 1448.91Z" fill="#FFDE88"/>
+        <path d="M429.404 1519.76V1471.96L800.196 1623.79V1672.15L429.404 1519.76Z" fill="#FFDE88"/>
+        <path d="M365.364 1590.61V1542.81L736.156 1694.64V1743L365.364 1590.61Z" fill="#FFDE88"/>
+        <path d="M493.444 1401.11L557.484 1376.93L928.276 1529.32L864.236 1552.94L493.444 1401.11Z" fill="#DD6702"/>
+        <path d="M429.404 1472.52L493.444 1448.34L864.236 1600.73L800.196 1624.35L429.404 1472.52Z" fill="#DD6702"/>
+        <path d="M365.364 1543.38L429.404 1519.2L800.196 1671.59L736.156 1695.2L365.364 1543.38Z" fill="#DD6702"/>
+      </svg>
+      
+      {/* Overlay Text */}
+      <div className={styles.allLink}>
+        <div className={`${styles.overlayText} ${styles.text2}`}><a href="#">Courses</a></div>
+        <div className={`${styles.overlayText} ${styles.text3}`}><a href="#">Hostels</a></div>
+        <div className={`${styles.overlayText} ${styles.text4}`}><a href="#">Events</a></div>
+        <div className={`${styles.overlayText} ${styles.text5}`}><a href="#">Awards & Achievements</a></div>
+      </div>
+      
+      {/* Overlay Icons */}
+      <div className={`${styles.icon} ${styles.icon1}`}><img src="stairs_imgs/icon-1.png" alt="Icon 1"/></div>
+      <div className={`${styles.icon} ${styles.icon2}`}><img src="stairs_imgs/icon-2.png" alt="Icon 2"/></div>
+      <div className={`${styles.icon} ${styles.icon3}`}><img src="stairs_imgs/icon-3.png" alt="Icon 3"/></div>
+      <div className={`${styles.icon} ${styles.icon4}`}><img src="stairs_imgs/icon-4.png" alt="Icon 4"/></div>
+      <div className={`${styles.icon} ${styles.icon5}`}><img src="stairs_imgs/pin1.png" alt="Icon 5"/></div>
+      <div className={`${styles.icon} ${styles.icon6}`}><img src="stairs_imgs/pin1.png" alt="Icon 6"/></div>
+      <div className={`${styles.icon} ${styles.icon7}`}><img src="stairs_imgs/pin1.png" alt="Icon 7"/></div>
+      <div className={`${styles.icon} ${styles.icon8}`}><img src="stairs_imgs/pin1.png" alt="Icon 8"/></div>
+    </div>
+  );
+};
+
+export default stairs;
