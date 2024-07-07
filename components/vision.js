7601ba3f071fdfe8a30a27fcf9068627b5a09aea
harshil5506 <harshil.hitendra.may2006@gmail.com>
2024-07-07 12:59:51 +0530

    vision

---
+++ components/vision.js
@@ -0,0 +1,31 @@
+const vision = () => {
+    return(
+  <div>
+    <div className="container">
+      <svg viewBox="0 0 66.701 199.999" fill="none" xmlns="http://www.w3.org/2000/svg" width="66.701" height="199.999">
+        <path d="M20.322 14.359C16.552 7.343 11.417 4.818 0 0v199.999h66.701s-18.94 -29.663 -19.994 -50.935c-1.138 -22.974 15.966 -41.011 9.287 -63.284 -4.559 -15.201 -6.774 -13.564 -15.296 -23.573 -10.837 -12.729 -16.607 -40.832 -20.376 -47.848" fill="#FB6BB0"/>
+        <path d="M12.619 22.965C8.672 13.437 0 7.203 0 7.203V199.999h42.938s-9.997 -29.099 -9.287 -50.093c0.66 -19.516 11.136 -31.069 6.009 -50.093 -4.015 -14.898 -13.018 -18.822 -19.065 -33.208 -5.981 -14.229 -2.071 -29.386 -7.976 -43.639" fill="#42C2EE"/>
+      </svg>
+      <div className="content">
+        <div className="vision">
+          <h2>OUR VISION</h2>
+          <p>
+            At BBIT College, we are guided by a set of core values that shape our approach to education and community engagement. These values are the foundation upon which we build transformative learning experiences.
+          </p>
+        </div>
+        <div className="mission">
+          <h2>OUR MISSION</h2>
+          <ul>
+            <li>Prepare the student with strong fundamental concepts, analytical abilities & Problem solving skills.</li>
+            <li>Offer ambiance & Support to cultivate creativity & Innovation.</li>
+            <li>Disseminate quality training to enhance skills & Entrepreneurship for professional development.</li>
+            <li>Establish opportunities for the students and teachers for lifelong learning to meet the ever changing global technological standards.</li>
+            <li>Foster students to seek excellence in technical education.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div> 
+    );
+}
+export default vision;
