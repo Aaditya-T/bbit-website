22cb299436d801e93eb4fd6f7add70e62f26f154
harshil5506 <harshil.hitendra.may2006@gmail.com>
2024-07-09 11:19:40 +0530

    hostel main content

---
+++ pages/hostel.js
@@ -0,0 +1,63 @@
+
+const hostel = () => {
+    return(
+    <div className="hostelcontainer">
+      <div className="hostelheader">
+        <h1>GIRL'S HOSTEL</h1>
+      </div>
+      <div className="hostelimageContainer">
+        <div className="hostelimagePlaceholder">IMAGE</div>
+      </div>
+      <div className="hostelcontent">
+        <p>
+        The campus comprises of well-manufactured inn blocks, both for girls and boys. Every inn comprises of different wreckage, managerial office, warden quarter, a separate mess and obliges around 200 students. Notwithstanding very much outfitted rooms, the lodging is finished with facilities like association, exercise center and different comforts for games, amusement and stimulation. It likewise incorporates every minute of every day 24/7 water facilities and RO for drinking water. A close-by shopping complex, comprising of different shops outside, is situated in the region of the lodging pieces and takes into account the day by day needs of the students like basic supplies, stationery, clothing and grooming
+
+        </p>
+        <h2>Hostels Amenities:</h2>
+        <ol>
+          <li>1. The hostel rooms are spacious with natural light and ventilation. Each room has an attached toilet and bathroom</li>
+          <li>2. Each student is provided with a study table, chair</li>
+          <li>3. Each hostel has 24 hour electricity and water supply, water cooler, common telephone, and common TV room</li>
+        </ol>
+        <h2>Security:</h2>
+        <p>
+        The following arrangements have been made to ensure the security of the inmates of the hostels:
+        </p>
+        <ol>
+          <li>1. Security check at the main gate of each Hostel.</li>
+          <li>2. Individual security guard at each hostel building.</li>
+          <li>3. Lady Warden at girl's hostels.</li>
+        </ol>
+      </div>
+      <div className="hostelheader">
+        <h1>BOY'S HOSTEL</h1>
+      </div>
+      <div className="hostelimageContainer">
+        <div className="hostelimagePlaceholder">IMAGE</div>
+      </div>
+      <div className="hostelcontent">
+        <p>
+        The campus comprises of well-manufactured inn blocks, both for girls and boys. Boy's inn comprises of different wreckage, managerial office, warden quarter, a mess and obliges around 290 students. Notwithstanding very much outfitted rooms, the lodging is finished with facilities like association, different comforts for games. It likewise incorporates every minute of every day 24/7 hot & cold water facilities and drinking water. A close-by shopping complex, comprising of different shops outside, is situated in the region of the lodging pieces and takes into account the day by day needs of the students like basic supplies, stationery, clothing and grooming.
+        </p>
+        <h2>Hostels Amenities:</h2>
+        <ol>
+          <li>1. The hostel rooms are spacious with natural light and ventilation.</li>
+          <li>2. Each student is provided with a study table, cupboard, and chair.</li>
+          <li>3. Hostel has 24 hour electricity and water supply, water cooler and common TV room</li>
+        </ol>
+        <h2>Security:</h2>
+        <p>
+        The following arrangements have been made to ensure the security of the inmates of the hostels
+        </p>
+        <ol>
+          <li>1. Security check at the main gate of each Hostel.</li>
+          <li>2. Individual security guard at each hostel building.</li>
+        </ol>
+      </div>
+    </div>
+
+
+
+    );
+}
+export default hostel;
