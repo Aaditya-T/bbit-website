468e2dd4801d9b267f5cad2340f8709c48986f27
harshil5506 <harshil.hitendra.may2006@gmail.com>
2024-07-18 00:59:30 +0530

    admission process page done

---
+++ pages/admission.js
@@ -0,0 +1,135 @@
+const admission = () => {
+
+    return(
+        <>
+  <div className="bg-[#F3F2FF]">
+        <div className="ellipse1 w-[8vw]  h-[16vh] rounded-[20vh] bg-[#EAE6FF] ml-[3vw] mt-[0vw] bottom-[0.6vh] top-[35vh] absolute z-0 "></div>
+        <div className="ellipse2 w-[20vw] h-[40vh] rounded-[20vh] bg-[#EAE6FF] ml-[4vw] mt-[0vw] bottom-[0.6vh] top-[60vh] absolute z-0 "></div>
+        <div className="ellipse3 w-[20vw] h-[40vh] rounded-[20vh] bg-[#EAE6FF] ml-[73vw] mt-[0vw] bottom-[0.6vh] top-[55vh] absolute z-0"></div>
+        <div className="ellipse4 w-[15vw] h-[30vh] rounded-[20vh] bg-[#EAE6FF] ml-[78.5vw] mt-[0vw] bottom-[0.6vh] top-[3vh] absolute z-0"></div>
+        <div className="ellipse5 w-[17vw] h-[34vh] rounded-[20vh] bg-[#EAE6FF] -ml-[9vw] mt-[0vw] bottom-[0.6vh] top-[167vh] absolute z-0 mr-[2vh]"></div>
+        <div className="ellipse6 w-[15vw] h-[30vh] rounded-[20vh] bg-[#EAE6FF] ml-[60vw] mt-[0vw] bottom-[0.6vh] top-[172vh] absolute z-0"></div>
+        <div className="ellipse7 w-[30vw] h-[60vh] rounded-[35vh] bg-[#EAE6FF] ml-[63vw] mt-[0vw] bottom-[0.6vh] top-[170vh] absolute z-0"></div>  
+    <div className="p-[1.5vw]  z-30 absolute">-
+      <div className="">
+        <div className="mb-[4vw] bg-[#FFDE88] mx-[33vw] rounded-[1.7vw] ml-[35vw] ">
+        <h1 className="text-center text-[1.8vw] font-bold py-[1.3vw] text-[#27066F] font-[OrelegaOne] ">ADMISSION PROCESS</h1>
+        </div>
+        <div className=" relative ml-[8vw] bg-gradient-to-b from-[#BCACFF] to-[#F3F2FF] p-4 rounded-md  w-[80vw] h-[40vh] text-[#27066F] mb-[4vw] ">
+          <ul className="list-disc list-inside leading-10 font-semibold text-left pl-[2vw]">
+            <li>We follow guideline given by state government for admission in Under Graduate courses.</li>
+            <li>As per state rules from total 75% seats are filled by Admission committee for professional Diploma Courses - ACPDC (www.acpdc.co.in) and remaining 25% seats, the 10% seats are filled under Management quota and 15% seats are filled under NRI/NRII sponsored quota.</li>
+            <li>We follow ACPDC guidelines for giving admission under management quota and NRI/NRII sponsored quota.</li>
+          </ul>
+        </div>
+        <div className="mb-4 bg-[#FFDE88] mx-[30vw] font-semibold rounded-[1.5vw] ">
+        <h2 className="text-center text-xl font-bold text-[#27066F] font-[OrelegaOne] py-[0.8vw] px-[0.8vw] mb-[4vw]">AICTE APPROVED INTAKE FOR 2023-24</h2>
+        </div>
+        <div>
+          <table className=" ml-[15vw] text-left border-collapse mb-8 text-[#27066F] w-[65vw] h-[65vh] ">
+            <thead>
+              <tr>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Sr.No.</th>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">CoursGRANT-IN-AID(GIA) COURSES OFFERED</th>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Annual Intake</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">1</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Civil Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">2</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Computer Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">3</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Electrical Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">4</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Electronics & Communication Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">5</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Mechanical Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">120</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 font-bold bg-[#EAE6FF] text-center" colSpan="2">TOTAL</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 font-bold bg-[#EAE6FF]">360</td>
+              </tr>
+            </tfoot>
+          </table>  
+          </div>
+
+        <div className="">
+          
+          <table className=" ml-[15vw] text-left border-collapse w-[65vw] h-[65vh]">
+            <thead>
+              <tr>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Sr.No.</th>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">SELF FINANCED(SF) COURSES OFFERED</th>
+                <th className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Annual Intake</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">1</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Civil Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">2</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Computer Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">120</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">3</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Electrical Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">4</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Electronics & Communication Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">5</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Information Technology</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">60</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">6</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Mechanical Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">120</td>
+              </tr>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">7</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">Diploma in Mechatronics Engineering</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 bg-[#EAE6FF]">30</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td className="border-[0.15vw] border-[#27066F] p-2 font-bold bg-[#EAE6FF] text-center" colSpan="2">TOTAL</td>
+                <td className="border-[0.15vw] border-[#27066F] p-2 font-bold bg-[#EAE6FF]">420</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
+    </div>
+        </>
+    )
+}
+
+export default admission;
