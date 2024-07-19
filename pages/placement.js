50e574b229c3709972c4e997904fd6edb6b31b8f
harshil5506 <harshil.hitendra.may2006@gmail.com>
2024-07-19 20:18:57 +0530

    placement page done

---
+++ pages/placement.js
@@ -0,0 +1,63 @@
+const placement = () => {
+  return (
+    <>
+    <div className="bg-[#F3F2FF]">
+        <div className="ellipse1 w-[12vw]  h-[24vh] rounded-[20vh] bg-[#EAE6FF] ml-[0.3vw] mt-[0vw] bottom-[0.6vh] top-[52vh] absolute z-0 "></div>
+        <div className="ellipse2 w-[20vw] h-[40vh] rounded-[20vh] bg-[#EAE6FF] ml-[4vw] mt-[0vw] bottom-[0.6vh] top-[95vh] absolute z-0 "></div>
+        <div className="ellipse3 w-[20vw] h-[40vh] rounded-[20vh] bg-[#EAE6FF] ml-[73vw] mt-[0vw] bottom-[0.6vh] top-[85vh] absolute z-0"></div>
+        <div className="ellipse4 w-[15vw] h-[30vh] rounded-[20vh] bg-[#EAE6FF] ml-[83.5vw] mt-[0vw] bottom-[0.6vh] top-[7vh] absolute z-0"></div>
+        <div className="ellipse5 w-[17vw] h-[34vh] rounded-[20vh] bg-[#EAE6FF] -ml-[3vw] mt-[0vw] bottom-[0.6vh] top-[167vh] absolute z-0 mr-[2vh]"></div>
+        <div className="ellipse6 w-[15vw] h-[30vh] rounded-[20vh] bg-[#EAE6FF] ml-[60vw] mt-[0vw] bottom-[0.6vh] top-[172vh] absolute z-0"></div>
+        <div className="ellipse7 w-[30vw] h-[60vh] rounded-[35vh] bg-[#EAE6FF] ml-[68vw] mt-[0vw] bottom-[0.6vh] top-[220vh] absolute z-0"></div> 
+    <div className="py-[3vw] z-50">
+      <div className="px-[1.3vw]">
+      <div className="mb-[4vw] bg-[#FFDE88] mx-[33vw] rounded-[1.7vw]  ">
+        <h1 className="text-center text-[1.8vw] font-bold py-[1.3vw] text-[#27066F] font-[OrelegaOne] ">Training & Placement Cell</h1>
+        </div>
+        <div className=" relative ml-[8vw] bg-gradient-to-b from-[#BCACFF] to-[#F3F2FF] p-[1.5vw] rounded-md  w-[80vw] h-[90vh] text-[#27066F] mb-[4vw] ">
+          <ul className="list-disc list-inside leading-8 font-semibold text-left pl-[2vw] ">
+            <li className="mb-[2vw] ">The Training and Placement Cell at B & B Institute of Technology (BBIT) works hard to support students by giving them opportunities to showcase their talents to various companies. It aims to provide the best platform for students by reaching out to top companies on their behalf</li>
+            <li className="mb-[2vw] ">BBIT's placement numbers and the number of recruiters have grown significantly. However, along with placements, BBIT also emphasizes moral qualities, ethics, and values.</li>
+            <li className="mb-[2vw] ">Each year, the Training and Placement Cell achieves great results with various organizations, including startups and multinational companies. This helps graduates find and seize opportunities in the industrial world.</li>
+            <li className="mb-[2vw] ">The BBIT Training and Placement Cell provides companies with a professional environment for conducting campus drives. This includes two fully equipped conference halls, a seminar hall that can hold 150 students, an auditorium with a capacity of 450 students, laboratories, and necessary classrooms with high-speed internet on the BBIT campus.</li>
+          </ul>
+        </div>
+        <div className="mb-[4vw] bg-[#FFDE88] mx-[33vw] rounded-[1.7vw]  z-50 ">
+        <h1 className="text-center text-[1.8vw] font-bold py-[1.3vw] text-[#27066F] font-[OrelegaOne] ">Associated Recruiters</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-7 mb-10 px-[8vw] relative">
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9]  w-[19vw] h-[15vw] rounded-lg shadow-md"></div>
+        </div>
+        <div className="mb-[4vw] bg-[#FFDE88] mx-[33vw] rounded-[1.7vw]">
+        <h1 className="text-center text-[1.8vw] font-bold py-[1.3vw] text-[#27066F] font-[OrelegaOne] ">Placement Gallery</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-7 px-[10vw]  relative ">
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+          <div className="bg-[#D9D9D9] h-[18vw] w-[23vw] rounded-lg shadow-md"></div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default placement;
